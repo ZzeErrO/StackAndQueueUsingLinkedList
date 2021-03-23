7e6bc00419e6e078972ab3382a47802e23577ecc
prashikjaware <prashikjaware123@gmail.com>
2021-03-23 18:43:42 +0530

    [Prashik]Added: Using Method Enqueue get element in Queue

--- DataStructure/StackAndQueue/Program.cs
+++ DataStructure/StackAndQueue/Program.cs
@@ -20,6 +20,13 @@ namespace StackAndQueue
             linkedListStack.Pop();
             linkedListStack.IsEmpty();
             linkedListStack.Display();
+
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+
             Console.ReadKey();
         }
 
